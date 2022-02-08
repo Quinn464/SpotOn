587221e0e6719114e36ab0ab536d3edc583c4153
unknown <quinnasempsrott@gmail.com>
2022-02-08 18:46:57 -0500

    first commit

---
+++ SpotOn.Data/Playlist.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotOn.Data
+{
+    public class Playlist 
+    {
+        [Key]
+        public int PaylistId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+        
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+       
+        //Foreign Key to Post via Id w/virtual Post)
+
+        [ForeignKey(nameof(Artist))]
+        public int ArtistId { get; set; }
+
+        public virtual Artist Artist { get; set; }
+
+        
+
+
+
+
+    }
+}
---
+++ SpotOn/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SpotOn.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}

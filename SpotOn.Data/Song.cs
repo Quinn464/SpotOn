64a5f7f011b7fd0a7e75443db39d00b62ee8f3eb
Corey Gossett <coreymgossett@gmail.com>
2022-02-12 09:59:58 -0500

    Song Data Layer

---
+++ SpotOn.Data/Song.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotOn.Data
+{
+    public enum GenreType
+    {
+        Rock,
+        Pop,
+        Country,
+        Blues,
+        Electronic,
+        Dance,
+        Jazz,
+        Dubstep,
+        IndieRock,
+    }
+    public class Song
+    {
+        [Key]
+        public int SongId { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        //[ForeignKey(nameof(Album))]
+        public int AlbumId { get; set; }
+        //[ForeignKey(nameof(Artist))]
+        public int ArtistId { get; set; }
+        public GenreType Genre { get; set; }
+    }
+}

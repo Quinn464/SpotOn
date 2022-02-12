19d1be99c0b2e50f257b80fde84be01c3363547b
Marleny Morales <marleny413@gmail.com>
2022-02-12 10:30:31 -0500

    UserDataLayer

--- SpotOn.Data/User.cs
+++ SpotOn.Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace SpotOn.Data
 {
     public class User
     {
+        [Key] 
+       public int UserId { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }

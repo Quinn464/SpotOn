ffecce5a3efaf82e824e8c61757482a133a6e6a2
Marleny Morales <marleny413@gmail.com>
2022-02-12 12:33:32 -0500

    Podcast

---
+++ SpotOn.Data/Podcast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotOn.Data
+{
+    class Podcast
+    {
+        [Key]
+        public int PodcastId { get; set; }
+        public int ArtistId { get; set; }
+        public GenreType Genre { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}

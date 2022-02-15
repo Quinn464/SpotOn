be3e2b246ace6b47a7240a33ecdbc0f4e496551b
Marleny Morales <marleny413@gmail.com>
2022-02-15 18:45:53 -0500

    saving

--- SpotOn.Models/PodcastCreate.cs
+++ SpotOn.Models/PodcastCreate.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SpotOn.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,7 @@ namespace SpotOn.Models
 {
     class PodcastCreate
     {
+     
+
     }
 }

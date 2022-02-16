68b82e0f5eddf906d7a92cc2b140b0f94b3c90fc
Corey Gossett <coreymgossett@gmail.com>
2022-02-15 19:36:23 -0500

    Added Song Model Layer

---
+++ SpotOn.Models/SongCreate.cs
@@ -0,0 +1,20 @@
+﻿using SpotOn.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotOn.Models
+{
+    public class SongCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        public GenreType Genre { get; set; }
+    }
+}
---
+++ SpotOn.Models/SongEdit.cs
@@ -0,0 +1,17 @@
+﻿using SpotOn.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotOn.Models
+{
+    public class SongEdit
+    {
+        public string Name { get; set; }
+        public GenreType Genre { get; set; }
+        public bool IsDeleted { get; set; }
+        public int ArtistId { get; set; }
+    }
+}

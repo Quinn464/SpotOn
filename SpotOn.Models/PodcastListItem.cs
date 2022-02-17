06da4212791218b56cf13ff087b998dd99454135
Marleny Morales <marleny413@gmail.com>
2022-02-16 20:56:02 -0500

    MPodcastModelsNew

--- SpotOn.Models/PodcastListItem.cs
+++ SpotOn.Models/PodcastListItem.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SpotOn.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace SpotOn.Models
 {
     class PodcastListItem
     {
+        public int PodcastId { get; set; }
+        public string Name { get; set; }
+
+        public GenreType Genre { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
     }
 }
